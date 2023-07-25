acb5fd57cdb28b1681874ef28c3390628f82ebee
Bear <danekgdgdg@gmail.com>
2023-07-25 23:46:15 +0300

    "Test"

---
+++ js/util.js
@@ -0,0 +1,23 @@
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback(...rest), timeoutDelay);
+  };
+};
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+export {
+  isEscapeKey,
+  debounce,
+  shuffle
+};
